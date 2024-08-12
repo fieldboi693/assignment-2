6daeb0aed2478a80b2efd9bd0f16db8e9549d5e6
fieldboi693 <fieldboi248@gmail.com>
2024-08-12 13:20:29 -0700

    first commit

---
+++ new_assignment.py
@@ -0,0 +1,20 @@
+# number = input("Enter a number: ")
+
+# if number > 0:
+#     print("The number is positive.")
+# elif number = 0:
+#     print("The number is zero.")
+# else number < 0:
+#     print("The number is negative.")
+
+
+"correct code"
+
+number = int(input( "Enter number: "))
+
+if number >0:
+    print("the number is positive.")
+elif number  ==0:
+    print("the number is zero.")
+else:
+    print("the number is negative.")
